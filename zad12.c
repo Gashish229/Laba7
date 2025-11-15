4de01786ec7c92a32c604eeda8bd632aa8dda49e
Gashish229 <SINKOV07@BK.RU>
2025-11-15 14:50:53 +0700

    Update zad12.c

--- zad12.c
+++ zad12.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
-double garmonic_chisel(double a, double b)
-{
-    if (a == 0 || b == 0)
-    {
-        printf("Числа не должны быть нулевыми\n");
-        return 0;
-    }
-    return 2.0 * a * b / (a + b);
-}
-
+#include<wchar.h>
+#include<locale.h>
+#include<limits.h>
 int main(void)
 {
-    double chisel1, chisel2;
-    printf("Введите два числа: ");
-    scanf("lf lf", &chisel1, &chisel2);
-    double resultat = garmonic_chisel(chisel1, chisel2);
-    printf("Среднее гармоническое значение чисел %.2f и %.2f = %.2f\n", chisel1, chisel2, resultat);
+    setlocale(LC_ALL, "");
+    unsigned long long count = 0;
+    wint_t ch;
+    wprintf(L"Введите текст(Ctrl+D для завершения): ");
+    while((ch = getwchar()) != WEOF)
+    {
+        if (count == ULLONG_MAX)
+        {
+            wprintf("!!Достигнут максимальный предел счётчика!!\n");
+            break;
+        }
+        count++;
+    }
+    wprintf(L"Кол-во символов равно: %llu\n", count);
     return 0;
 }
